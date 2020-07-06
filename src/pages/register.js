05ec1a246cf6e42817baf8ae035b1f3e66436a3a
RafaelCenzano <savagecoder77@gmail.com>
2020-07-06 16:39:21 -0700

    remove unused code

--- src/pages/register.js
+++ src/pages/register.js
@@ -1,20 +1,4 @@
 import React from 'react'
-import styled from 'styled-components'
-import {GlobalStyle} from 'theme'
-
-const Div = styled.div`
-  margin: 0;
-  height: 100vh;
-  overflow: hidden;
-  iframe{
-    position: absolute;
-    left:0;
-    right:0;
-    bottom:0;
-    top:0;
-    border:0;
-  }
-`;
 
 export default (props) => {
   return (
